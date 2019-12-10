959bb1f7bb5fbbf86ecad38accde834ead196e9e
Miral Doshi <miraldoshi@Zipoders-MacBook-Pro-2.local>
2019-12-09 22:08:46 -0500

    arrays and fundamentals done

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -1,22 +1,37 @@
 package rocks.zipcode.quiz4.arrays;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author leon on 01/01/2019.
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+
+       return values[values.length / 2];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        List<String> middle = new ArrayList<String>(Arrays.asList(values));
+        middle.remove(getMiddleElement(values));
+
+        String[] finalArray = middle.toArray(new String[middle.size()]);
+        return finalArray;
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length-1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        List<String> lastelement = new ArrayList<String>(Arrays.asList(values));
+        lastelement.remove(values.length-1);
+        String[] lastremoved = lastelement.toArray(new String[lastelement.size()]);
+        return lastremoved;
     }
+
+
 }
--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,27 +1,47 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+
+        return string.charAt(string.length()/2);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+      int middle = str.length()/2;
+        return str.substring(0,middle) + Character.toUpperCase(getMiddleCharacter(str)) + str.substring(middle + 1);
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+
+        int middle = str.length()/2;
+        return str.substring(0,middle) + Character.toLowerCase(getMiddleCharacter(str)) + str.substring(middle + 1);
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+        int length = str.length();
+        char arr[] = str.toCharArray();
+
+        Arrays.sort(arr);
+        for(int i = 0; i < length - 1; i++){
+            if (arr[i] == arr[i +1])
+                return false;
+        }
+        return true;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
@@ -29,6 +49,15 @@ public class StringUtils {
     }
 
     public static String invertCasing(String str) {
-        return null;
+        String [] splitString = str.split("");
+        String result = "";
+        for (String s :splitString){
+            if (Character.isUpperCase(s.charAt(0))){
+                result += s.toLowerCase();
+            } else {
+                result +=s.toUpperCase();
+            }
+        }
+        return result;
     }
 }
